3619112534884f4efb435dd2875eee811f356bcd
Peiyong Wang <peiyong.wang@csiro.au>
2025-03-26 13:24:06 +1100

    add run_experiments.sh: script to execute multiple PPO training scripts with pretrained models

---
+++ scripts/run_experiments.sh
@@ -0,0 +1,6 @@
+python ppo_classicalNN_atari.py
+
+python ppo_separable_atari.py --pretrained_backbone_path "/home/wan455/Desktop/Projects/QRL-Pong/scripts/trained-models/classical_backbone_(sine_activation)_PongNoFrameskip-v4_latent_dim_12_seed_1.pt"
+python ppo_entangled_atari.py --pretrained_backbone_path "/home/wan455/Desktop/Projects/QRL-Pong/scripts/trained-models/classical_backbone_(sine_activation)_PongNoFrameskip-v4_latent_dim_12_seed_1.pt"
+
+python ppo_classicalNNSineless_atari.py
