e432a9c3cd50f0e0d888217b0eac5bc827c3712a
Peiyong Wang <peiyong.wang@csiro.au>
2025-03-29 13:00:31 +1100

    update run_experiments.sh to use sineless backbone for separable and entangled models

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -1,6 +1,2 @@
-python ppo_classicalNN_atari.py
-
-python ppo_separable_atari.py --pretrained_backbone_path "/home/wan455/Desktop/Projects/QRL-Pong/scripts/trained-models/classical_backbone_(sine_activation)_PongNoFrameskip-v4_latent_dim_12_seed_1.pt"
-python ppo_entangled_atari.py --pretrained_backbone_path "/home/wan455/Desktop/Projects/QRL-Pong/scripts/trained-models/classical_backbone_(sine_activation)_PongNoFrameskip-v4_latent_dim_12_seed_1.pt"
-
-python ppo_classicalNNSineless_atari.py
+python ppo_separable_sineless_backbone_atari.py --pretrained_backbone_path "/home/wan455/Desktop/Projects/QRL-Pong/scripts/trained-models/classical_backbone_(sineless)_PongNoFrameskip-v4_latent_dim_12_seed_1.pt"
+python ppo_entangled_sineless_backbone_atari.py --pretrained_backbone_path "/home/wan455/Desktop/Projects/QRL-Pong/scripts/trained-models/classical_backbone_(sineless)_PongNoFrameskip-v4_latent_dim_12_seed_1.pt"
