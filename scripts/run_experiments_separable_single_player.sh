bf74af20cd971b62db2abe3819865392852a0774
Peiyong Wang <peiyong.wang@csiro.au>
2025-04-01 13:38:53 +1100

    increase the number of workers in run_experiments_separable_single_player.sh from 9 to 12

--- scripts/run_experiments_separable_single_player.sh
+++ scripts/run_experiments_separable_single_player.sh
@@ -2,4 +2,4 @@ python benchmark_utils.py \
     --env-ids MsPacmanNoFrameskip-v4 PongNoFrameskip-v4 BeamRiderNoFrameskip-v4 BreakoutNoFrameskip-v4 AirRaidNoFrameskip-v4 AmidarNoFrameskip-v4 AssaultNoFrameskip-v4 BowlingNoFrameskip-v4 SkiingNoFrameskip-v4 FreewayNoFrameskip-v4 AsterixNoFrameskip-v4 SpaceInvadersNoFrameskip-v4\
     --command "poetry run python ppo_separable_unclamped_atari_from_scratch.py" \
     --num-seeds 1 \
-    --workers 9 \
+    --workers 12 \
