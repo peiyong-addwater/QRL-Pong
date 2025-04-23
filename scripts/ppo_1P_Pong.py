b611c928c3b2ded7dc87f1fd4ec25e8d31e5bcfe
Peiyong Wang <peiyong.wang@csiro.au>
2025-04-23 13:42:04 +1000

    Add stable-baselines3 dependency and pandas package to project

---
+++ scripts/ppo_1P_Pong.py
@@ -0,0 +1,131 @@
+# Based on CleanRL's PPO implementation
+# https://docs.cleanrl.dev/rl-algorithms/ppo/#ppo_ataripy
+import os
+import random
+import time
+from dataclasses import dataclass
+
+import gymnasium as gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import tyro
+from torch.distributions.categorical import Categorical
+from torch.utils.tensorboard import SummaryWriter
+
+from stable_baselines3.common.atari_wrappers import (  # isort:skip
+    ClipRewardEnv,
+    EpisodicLifeEnv,
+    FireResetEnv,
+    MaxAndSkipEnv,
+    NoopResetEnv,
+)
+
+from model import ( # isort:skip
+    Backbone,
+    ClassicalPPOAgentWithPlaceholder,
+    ClassicalPPOAgentWithPlaceholderSineless,
+    EntangledPPOAgent,
+    SeparablePPOAgent
+)
+
+import ale_py
+gym.register_envs(ale_py)
+
+@dataclass
+class Args:
+    exp_name: str = "Pong1P" #os.path.basename(__file__)[: -len(".py")]
+    """the base name of the experiment"""
+    seed: int = 1
+    """random seed"""
+    torch_deterministic: bool = True
+    """if toggled, `torch.backends.cudnn.deterministic=False`"""
+    cuda: bool = True
+    """if toggled, cuda will be enabled by default"""
+    track: bool = True
+    """if toggled, this experiment will be tracked with Weights and Biases"""
+    wandb_entity: str = "addwater0315-csiro"
+    """the entity (team) of wandb's project"""
+    capture_video: bool = True
+    """whether to capture videos of the agent performances (check out `videos` folder)"""
+
+    # Agent specific arguments
+    agent_type: str = "classicalNNSineActor"
+    """the type of the agent, choose from ['classicalNNSineActor', 'classicalNNSinelessActor', 'entangled', 'separable'] """
+    backbone_out_dim: int = 12
+    """the output dimension of the backbone network"""
+    from_scratch: bool = True
+    """if toggled, the backbone network will be trained from scratch"""
+    pretrained_backbone_path: str = None
+    """the path to the pretrained backbone model"""
+    model_save_path: str = None
+    """the path to save the model"""
+    clamp_actor_weights: bool = False
+    """if toggled, the quantum actor weights will be clamped to [-pi, pi]. Only used for quantum-classical hybrid agents"""
+
+    # Algorithm specific arguments
+    env_id: str = "PongNoFrameskip-v4"
+    """the id of the environment"""
+    total_timesteps: int = 10000000
+    """total timesteps of the experiments"""
+    learning_rate: float = 2.5e-4
+    """the learning rate of the optimizer"""
+    num_envs: int = 8
+    """the number of parallel game environments"""
+    num_steps: int = 128
+    """the number of steps to run in each environment per policy rollout"""
+    anneal_lr: bool = True
+    """Toggle learning rate annealing for policy and value networks"""
+    gamma: float = 0.99
+    """the discount factor gamma"""
+    gae_lambda: float = 0.95
+    """the lambda for the general advantage estimation"""
+    num_minibatches: int = 4
+    """the number of mini-batches"""
+    update_epochs: int = 4
+    """the K epochs to update the policy"""
+    norm_adv: bool = True
+    """Toggles advantages normalization"""
+    clip_coef: float = 0.1
+    """the surrogate clipping coefficient"""
+    clip_vloss: bool = True
+    """Toggles whether or not to use a clipped loss for the value function, as per the paper."""
+    ent_coef: float = 0.01
+    """coefficient of the entropy"""
+    vf_coef: float = 0.5
+    """coefficient of the value function"""
+    max_grad_norm: float = 0.5
+    """the maximum norm for the gradient clipping"""
+    target_kl: float = None
+    """the target KL divergence threshold"""
+
+    # to be filled in runtime
+    batch_size: int = 0
+    """the batch size (computed in runtime)"""
+    minibatch_size: int = 0
+    """the mini-batch size (computed in runtime)"""
+    num_iterations: int = 0
+    """the number of iterations (computed in runtime)"""
+
+def make_env(env_id, idx, capture_video, run_name):
+    def thunk():
+        if capture_video and idx == 0:
+            env = gym.make(env_id, render_mode="rgb_array")
+            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+        else:
+            env = gym.make(env_id)
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        env = NoopResetEnv(env, noop_max=30)
+        env = MaxAndSkipEnv(env, skip=4)
+        env = EpisodicLifeEnv(env)
+        if "FIRE" in env.unwrapped.get_action_meanings():
+            env = FireResetEnv(env)
+        env = ClipRewardEnv(env)
+        env = gym.wrappers.ResizeObservation(env, (84, 84))
+        env = gym.wrappers.GrayscaleObservation(env)
+        env = gym.wrappers.FrameStackObservation(env, 4)
+        return env
+
+    return thunk
+
