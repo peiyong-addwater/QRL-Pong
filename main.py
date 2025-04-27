cffdd9bb8c31b199a77a7e220bcc30e5532eb565
Peiyong Wang <addwater0315@gmail.com>
2025-04-27 13:58:28 +0000

    Refactor environment setup to use Pong with Gym wrappers for observation processing

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-import supersuit
-from pettingzoo.atari import space_invaders_v2
+from pettingzoo.atari import pong_v3
+import gymnasium as gym
 
-env = space_invaders_v2.env()
+env = pong_v3.parallel_env(render_mode="human")
+env = gym.wrappers.ResizeObservation(env, (84, 84))
+env = gym.wrappers.GrayscaleObservation(env)
+env = gym.wrappers.FrameStackObservation(env, 4)
+observations, infos = env.reset()
 
-# as per openai baseline's MaxAndSKip wrapper, maxes over the last 2 frames
-# to deal with frame flickering
-env = supersuit.max_observation_v0(env, 2)
+while env.agents:
+    # this is where you would insert your policy
+    actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+    print(actions)
+    observations, rewards, terminations, truncations, infos = env.step(actions)
+    print(observations)
+    print(rewards)
+    print(terminations)
+    print(truncations)
+    print(infos)
+    break
+env.close()
 
-# repeat_action_probability is set to 0.25 to introduce non-determinism to the system
-env = supersuit.sticky_actions_v0(env, repeat_action_probability=0.25)
-
-# skip frames for faster processing and less control
-# to be compatible with gym, use frame_skip(env, (2,5))
-env = supersuit.frame_skip_v0(env, 4)
-
-# downscale observation for faster processing
-env = supersuit.resize_v1(env, 84, 84)
-
-# allow agent to see everything on the screen despite Atari's flickering screen problem
-env = supersuit.frame_stack_v1(env, 4)
