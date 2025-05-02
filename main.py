7368edd956557876ae1eedb82dda5b551f23172a
Peiyong Wang <addwater0315@gmail.com>
2025-05-02 15:20:30 +0000

    Enhance PPO training script for 2-player Pong by adding agent initialization and environment setup

--- main.py
+++ main.py
@@ -8,21 +8,27 @@ env = ss.clip_reward_v0(env, lower_bound=-1, upper_bound=1)
 env = ss.color_reduction_v0(env, mode="B")
 env = ss.resize_v1(env, x_size=84, y_size=84)
 env = ss.frame_stack_v1(env, 4)
-#env = ss.agent_indicator_v0(env, type_only=False)
+env = ss.agent_indicator_v0(env, type_only=False)
 #env = ss.pettingzoo_env_to_vec_env_v1(env)
 #env = ss.concat_vec_envs_v1(env, 16 // 2, num_cpus=0, base_class="gymnasium")
-#env.single_observation_space = env.observation_space
-# print(envs.single_observation_space)
-#env.single_action_space = env.action_space
-#env.is_vector_env = True
+env.single_observation_space = env.observation_space('first_0')
+env.single_action_space = env.action_space('first_0')
+print(env.single_observation_space)
+print(env.single_action_space.n)
+#print(env.observation_space('first_0'))
+#print(env.action_space('first_0'))
+#print(env.observation_space('second_0'))
+#print(env.action_space('second_0'))
+env.is_vector_env = True
 observations, infos = env.reset()
-
+#print(env.agents)
+print(env.action_space)
 while env.agents:
     # this is where you would insert your policy
     actions = {agent: env.action_space(agent).sample() for agent in env.agents}
     print(actions)
     observations, rewards, terminations, truncations, infos = env.step(actions)
-    print(observations)
+    #print(observations)
     print(rewards)
     print(terminations)
     print(truncations)
--- scripts/ppo_2P_Pong.py
+++ scripts/ppo_2P_Pong.py
@@ -87,3 +87,73 @@ class Args:
     """the mini-batch size (computed in runtime)"""
     num_iterations: int = 0
     """the number of iterations (computed in runtime)"""
+
+if __name__ == "__main__":
+    args = tyro.cli(Args)
+    args.num_envs = 1
+    args.n_layers = int(args.backbone_out_dim**2/(args.backbone_out_dim/3*3))
+    args.batch_size = int(args.num_envs * args.num_steps)
+    args.batch_size = int(args.num_envs * args.num_steps)
+    args.minibatch_size = int(args.batch_size // args.num_minibatches)
+    args.num_iterations = args.total_timesteps // args.batch_size
+    args.wandb_project_name = f"Pong2P__Dim__{args.backbone_out_dim}"
+
+    assert args.backbone_out_dim / 3 == int(args.backbone_out_dim / 3), "backbone_out_dim must be a multiple of 3"
+
+    run_name = f"Pong2P__Dim__{args.backbone_out_dim}__Seed__{args.seed}__{int(time.time())}"
+
+    print(args)
+
+    if args.track:
+        import wandb
+
+        wandb.init(
+            project=args.wandb_project_name,
+            entity=args.wandb_entity,
+            sync_tensorboard=True,
+            config=vars(args),
+            name=run_name,
+            monitor_gym=False,
+            save_code=False,
+        )
+    
+    writer = SummaryWriter(f"runs/{run_name}")
+    writer.add_text(
+        "hyperparameters",
+        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    )
+
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+
+    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+
+    # env setup
+    env = pong_v3.parallel_env(render_mode="human")
+    env = ss.frame_skip_v0(env, 4)
+    env = ss.clip_reward_v0(env, lower_bound=-1, upper_bound=1)
+    env = ss.color_reduction_v0(env, mode="B")
+    env = ss.resize_v1(env, x_size=84, y_size=84)
+    env = ss.frame_stack_v1(env, 4)
+    env.single_action_space = env.action_space('first_0')
+    env.single_observation_space = env.observation_space('first_0')
+
+    # agents
+    separableAgent = SeparablePPOAgent(env, n_layers=args.n_layers, backbone_out_dim=args.backbone_out_dim, pretrained_backbone=False, backbone = Backbone(out_dim = args.backbone_out_dim)).to(device) # 'first_0'
+    entangledAgent = EntangledPPOAgent(env, n_layers=args.n_layers, backbone_out_dim=args.backbone_out_dim, pretrained_backbone=False, backbone = Backbone(out_dim = args.backbone_out_dim)).to(device) # 'second_0'
+    print("Agents created...\n")
+
+    # optimiser
+    optimizerS = optim.Adam(separableAgent.parameters(), lr=args.learning_rate, eps=1e-5)
+    optimizerE = optim.Adam(entangledAgent.parameters(), lr=args.learning_rate, eps=1e-5)
+
+    # ALGO Logic: Storage setup
+
+
+
+
+
+    
