b018f432bd24768630fc3a22525897014f3d52fe
Peiyong Wang <peiyong.wang@csiro.au>
2025-05-19 12:21:34 +1000

    finished some other run

--- scripts/benchmarkPong2PCompete.py
+++ scripts/benchmarkPong2PCompete.py
@@ -25,7 +25,7 @@ def run_experiment(command: str):
 
 command_list = []
 
-for i in range(16, 20):
+for i in range(21, 30):
     command_list.append(
         f"uv run pong2p_compete.py --seed {i} --clamp_actor_weights"
     )
