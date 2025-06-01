5e54db1208aac6aa48edcec7b59cd11a231dbc26
Peiyong Wang <addwater0315@gmail.com>
2025-06-01 16:33:46 +0000

    Create directory for Pong1P models if it doesn't exist

--- scripts/ppo_1P_Pong.py
+++ scripts/ppo_1P_Pong.py
@@ -172,6 +172,8 @@ if __name__ == "__main__":
     if args.model_save_path is None:
         if not os.path.exists("trained-models"):
             os.makedirs("trained-models")
+            if not os.path.exists(os.path.join("trained-models", "Pong1PModels")):
+                os.makedirs(os.path.join("trained-models", "Pong1PModels"))
         args.model_save_path = f"trained-models/{run_name}.pt"
 
     print(args)
