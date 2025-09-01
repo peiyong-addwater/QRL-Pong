fefa5cebbb9c6d91701dfdb7d98b8dc50da42537
Peiyong Wang <Peiyong.Wang@csiro.au>
2025-09-01 14:33:34 +1000

    updates

---
+++ PongAgents/VQCBackbones2.py
@@ -0,0 +1,344 @@
+import pennylane as qml
+
+import torch
+import torch.nn as nn
+
+import numpy as np
+
+from typing import List, Tuple, Union, Callable
+
+ket0bra0 = np.outer(np.array([1., 0.]), np.array([1., 0.]))
+
+def controlledF(theta, wires):
+    """
+    The controlled F gate as in https://arxiv.org/abs/1606.09290
+    """
+    qml.QubitUnitary([[1, 0, 0, 0],[0, 1, 0, 0],[0, 0, np.cos(theta*2), np.sin(theta*2)],[0, 0, np.sin(theta*2), -np.cos(theta*2)]], wires=wires)
+
+def three_qubit_W(n=3, wires:List[int]=None):
+    """
+    Creates a three-qubit W state.
+    The initial state is assumed to be |100>
+    """
+    assert n == 3, "In this circuit W state is only defined for 3 qubits. Got: {}".format(n)
+    if wires is None:
+        wires = [0, 1, 2]
+    assert len(wires) == 3, "Wires must have length 3. Got: {}".format(len(wires))
+    
+    theta_12 = np.arccos(np.sqrt(1/n))/2
+    theta_23 = np.arccos(np.sqrt(1/(n-1)))/2
+
+    controlledF(theta_12, wires=[wires[0], wires[1]])
+    qml.CNOT(wires=[wires[1], wires[0]])
+    controlledF(theta_23, wires=[wires[1], wires[2]])
+    qml.CNOT(wires=[wires[2], wires[1]])
+
+def create_W_state(qubit_list: List[int]):
+    """
+    Creates a W state on the specified qubits.
+    The initial state is assumed to be |1000...0>
+    """
+    assert len(qubit_list) >= 3, "W state requires at least 3 qubits. Got: {}".format(len(qubit_list))
+
+
+    if len(qubit_list) == 3:
+        three_qubit_W(wires=qubit_list)
+    else:
+        theta_12 = np.arccos(np.sqrt(1/len(qubit_list)))/2
+        controlledF(theta_12, wires=[qubit_list[0], qubit_list[1]])
+        create_W_state(qubit_list[1:])
+        for i in range(1, len(qubit_list)):
+            qml.CNOT(wires=[qubit_list[i], qubit_list[0]])
+        
+def create_GHZ_state(qubit_list: List[int]):
+    """
+    Creates a GHZ state on the specified qubits.
+    """
+    qml.Hadamard(qubit_list[0])
+    for qubit in qubit_list[1:]:
+        qml.CNOT(wires=[qubit_list[0], qubit])
+
+def create_graph_state(qubit_list: List[int], edges: List[Tuple[int, int]]):
+    """
+    Creates a graph state on the specified qubits with given edges.
+    """
+    for qubit in qubit_list:
+        qml.Hadamard(qubit)
+    for edge in edges:
+        node_1, node_2 = edge
+        assert node_1 in qubit_list and node_2 in qubit_list, "Both nodes must be in the qubit list. Got: {}, {}".format(node_1, node_2)
+        qml.CZ(wires=[node_1, node_2])
+
+def V_i_l(x_i, theta_0, theta_1, theta_2, bias_0, bias_1, bias_2, qubit):
+    """
+    Single-qubit PQC for data-encoding and parameterized gates.
+    """
+    qml.U3(theta_0*x_i+bias_0, theta_1*x_i+bias_1, theta_2*x_i+bias_2, wires=qubit)
+
+
+def make_entangled_circ_ghz(n_layers: int)->Callable:
+    """
+    Creates a parameterised circuit starting with a GHZ state.
+    """
+
+    assert n_layers > 1, "Number of layers must be greater than 1. Got: {}".format(n_layers)
+
+    device = qml.device("default.qubit", wires = 8)
+    qubit_list = [1, 0, 2, 3, 4, 5, 6, 7] # qubit #1 is the control qubit for GHZ state since it is the right paddle qubit
+    # measurement observables
+
+    def circuit(x, params):
+        """
+        input x has shape (, 8)
+        params has shape (n_layers, 8, 6)
+        """
+        create_GHZ_state(qubit_list)
+
+        for l in range(n_layers - 1):
+            for i in range(8):
+                V_i_l(x[...,i], params[l][i][0], params[l][i][1], params[l][i][2], params[l][i][3], params[l][i][4], params[l][i][5], i)
+
+        qml.adjoint(create_GHZ_state)(qubit_list)
+
+        return [qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1)), qml.expval(qml.PauliZ(2))]
+
+    compiled_circuit = qml.compile(circuit)
+    qnode = qml.QNode(compiled_circuit, device, interface='torch')
+
+    def qfunc(x, params):
+        """
+        QNode function that takes input x and parameters params.
+        """
+        assert x.shape[-1] == 8, "Input x must have shape (..., 8). Got: {}".format(x.shape)
+        assert params.shape == (n_layers, 8, 6), "Parameters must have shape (n_layers, 8, 6). Got: {}".format(params.shape)
+
+        circ_out = qnode(x, params)
+        circ_out = torch.stack(circ_out)
+        circ_out = torch.einsum("ij->ji", circ_out)
+        return circ_out
+    
+    return qfunc
+
+def make_entangled_circ_graph_state(n_layers: int, edge_list:List[Tuple[int, int]])->Callable:
+    """
+    Creates a parameterised circuit starting with a graph state.
+    """
+
+    assert n_layers > 1, "Number of layers must be greater than 1. Got: {}".format(n_layers)
+
+    qubit_list = [0, 1, 2, 3, 4, 5, 6, 7]
+    for edge in edge_list:
+        assert edge[0] in qubit_list and edge[1] in qubit_list, "Both nodes must be in the qubit list. Got: {}, {}".format(edge[0], edge[1])
+    
+    device = qml.device("default.qubit", wires = 8)
+    
+    def circuit(x, params):
+        """
+        input x has shape (, 8)
+        params has shape (n_layers, 8, 6)
+        """
+        create_graph_state(qubit_list, edge_list)
+
+        for l in range(n_layers - 1):
+            for i in range(8):
+                V_i_l(x[...,i], params[l][i][0], params[l][i][1], params[l][i][2], params[l][i][3], params[l][i][4], params[l][i][5], i)
+
+        qml.adjoint(create_graph_state)(qubit_list, edge_list)
+        
+        return [qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1)), qml.expval(qml.PauliZ(2))]
+    
+    compiled_circuit = qml.compile(circuit)
+    qnode = qml.QNode(compiled_circuit, device, interface='torch')
+    
+    def qfunc(x, params):
+        """
+        QNode function that takes input x and parameters params.
+        """
+        assert x.shape[-1] == 8, "Input x must have shape (..., 8). Got: {}".format(x.shape)
+        assert params.shape == (n_layers, 8, 6), "Parameters must have shape (n_layers, 8, 6). Got: {}".format(params.shape)
+        circ_out = qnode(x, params)
+        circ_out = torch.stack(circ_out)
+        circ_out = torch.einsum("ij->ji", circ_out)
+        return circ_out
+    
+    return qfunc
+
+def make_separable_circ(n_layers: int)->Callable:
+    """
+    Creates a parameterised circuit with no entanglement.
+    """
+    
+    assert n_layers > 1, "Number of layers must be greater than 1. Got: {}".format(n_layers)
+
+    device = qml.device("default.qubit", wires = 8)
+    
+    def circuit(x, params):
+        """
+        input x has shape (, 8)
+        params has shape (n_layers, 8, 6)
+        """
+        for l in range(n_layers - 1):
+            for i in range(8):
+                V_i_l(x[...,i], params[l][i][0], params[l][i][1], params[l][i][2], params[l][i][3], params[l][i][4], params[l][i][5], i)
+
+        return [qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1)), qml.expval(qml.PauliZ(2))]
+    
+    compiled_circuit = qml.compile(circuit)
+    qnode = qml.QNode(compiled_circuit, device, interface='torch')
+    
+    def qfunc(x, params):
+        """
+        QNode function that takes input x and parameters params.
+        """
+        assert x.shape[-1] == 8, "Input x must have shape (..., 8). Got: {}".format(x.shape)
+        assert params.shape == (n_layers, 8, 6), "Parameters must have shape (n_layers, 8, 6). Got: {}".format(params.shape)
+        circ_out = qnode(x, params)
+        circ_out = torch.stack(circ_out)
+        circ_out = torch.einsum("ij->ji", circ_out)
+        return circ_out
+    
+    return qfunc
+
+def make_entangled_circ_w_state(n_layers: int)->Callable:
+    """
+    Creates a parameterised circuit starting with a W state.
+    """
+
+    assert n_layers > 1, "Number of layers must be greater than 1. Got: {}".format(n_layers)
+
+    device = qml.device("default.qubit", wires = 8)
+    qubit_list = [0, 1, 2, 3, 4, 5, 6, 7] # qubit #1 is the control qubit for W state since it is the right paddle qubit
+    
+    def circuit(x, params):
+        """
+        input x has shape (, 8)
+        params has shape (n_layers, 8, 6)
+        """
+        # Create W state
+        qml.PauliX(wires=qubit_list[0])  # Initialize the first qubit to |1>
+        create_W_state(qubit_list)
+
+        for l in range(n_layers - 1):
+            for i in range(8):
+                V_i_l(x[...,i], params[l][i][0], params[l][i][1], params[l][i][2], params[l][i][3], params[l][i][4], params[l][i][5], i)
+
+        qml.adjoint(create_W_state)(qubit_list)
+        qml.PauliX(wires=qubit_list[0])  # Reset the first qubit to |0>
+
+
+        return [qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1)), qml.expval(qml.PauliZ(2))]
+    
+    compiled_circuit = qml.compile(circuit)
+    qnode = qml.QNode(compiled_circuit, device, interface='torch')
+    def qfunc(x, params):
+        """
+        QNode function that takes input x and parameters params.
+        """
+        assert x.shape[-1] == 8, "Input x must have shape (..., 8). Got: {}".format(x.shape)
+        assert params.shape == (n_layers, 8, 6), "Parameters must have shape (n_layers, 8, 6). Got: {}".format(params.shape)
+
+        circ_out = qnode(x, params)
+        circ_out = torch.stack(circ_out)
+        circ_out = torch.einsum("ij->ji", circ_out)
+        return circ_out
+    
+    return qfunc
+
+class WStateBackbone(nn.Module):
+    def __init__(self, n_layers, output_dim, observation_space, edge_list=None):
+        super().__init__()
+        self.output_dim = output_dim
+        self.observation_dim = observation_space
+        assert self.output_dim == 3 # only 3 output dim
+        assert self.observation_dim == 8 # paddly_yl, paddle_yr, ball_x, ball_y, ball_vx, ball_vy, score_l, score_r
+
+        self.qfunc = make_entangled_circ_w_state(n_layers)
+        self.params = nn.Parameter(
+            torch.rand((n_layers, 8, 6), requires_grad=True)
+            )
+    
+    def forward(self, x):
+        x = x * torch.pi
+        out = self.qfunc(x, self.params).to(x.dtype)
+        return out
+
+class GHZBackbone(nn.Module):
+    def __init__(self, n_layers, output_dim, observation_space, edge_list=None):
+        super().__init__()
+        self.output_dim = output_dim
+        self.observation_dim = observation_space
+        assert self.output_dim == 3 # only 3 output dim
+        assert self.observation_dim == 8 # paddly_yl, paddle_yr, ball_x, ball_y, ball_vx, ball_vy, score_l, score_r
+
+        self.qfunc = make_entangled_circ_ghz(n_layers)
+        self.params = nn.Parameter(
+            torch.rand((n_layers, 8, 6), requires_grad=True)
+            )
+    
+    def forward(self, x):
+        x = x * torch.pi
+        out = self.qfunc(x, self.params).to(x.dtype)
+        return out
+
+class GraphStateBackbone(nn.Module):
+    def __init__(self, n_layers, output_dim, observation_space, edge_list=None):
+        super().__init__()
+        self.output_dim = output_dim
+        self.observation_dim = observation_space
+        assert self.output_dim == 3 # only 3 output dim
+        assert self.observation_dim == 8 # paddly_yl, paddle_yr, ball_x, ball_y, ball_vx, ball_vy, score_l, score_r
+        assert edge_list is not None, "Edge list must be provided for GraphStateBackbone"
+
+        self.qfunc = make_entangled_circ_graph_state(n_layers, edge_list=edge_list)
+        self.params = nn.Parameter(
+            torch.rand((n_layers, 8, 6), requires_grad=True)
+            )
+
+    def forward(self, x):
+        x = x * torch.pi
+        out = self.qfunc(x, self.params).to(x.dtype)
+        return out
+
+class SeparableBackbone(nn.Module):
+    def __init__(self, n_layers, output_dim, observation_space, edge_list=None):
+        super().__init__()
+        self.output_dim = output_dim
+        self.observation_dim = observation_space
+        assert self.output_dim == 3 # only 3 output dim
+        assert self.observation_dim == 8 # paddly_yl, paddle_yr, ball_x, ball_y, ball_vx, ball_vy, score_l, score_r
+
+        self.qfunc = make_separable_circ(n_layers)
+        self.params = nn.Parameter(
+            torch.rand((n_layers, 8, 6), requires_grad=True)
+            )
+
+    def forward(self, x):
+        x = x * torch.pi
+        out = self.qfunc(x, self.params).to(x.dtype)
+        return out
+
+if __name__ == "__main__":
+    from pufferlib import vector
+    from pufferlib.ocean import env_creator
+
+    # Example usage
+    n_layers = 6
+    edge_list = [(3, 0), (2, 0), (6, 0), (4, 0), (5, 0), (3, 1), (2, 1), (4, 1), (5, 1), (7, 1), (0, 1)]
+    circuit = make_entangled_circ_w_state(n_layers)
+    
+    x = torch.tensor(np.random.rand(12, 8))
+    params = torch.tensor(np.random.rand(n_layers, 8, 6), requires_grad=True)
+    result = circuit(x, params)
+    print("Circuit output shape:", result.shape)
+    print("Circuit output:\n", result)
+
+    # W state example
+    wires = [0, 1, 2, 3]
+    dev = qml.device("default.qubit", wires=wires)
+    @qml.qnode(dev, interface = 'torch')
+    def w_state_circuit():
+        qml.PauliX(wires=wires[0])
+        create_W_state(wires)
+        return qml.probs()
+
+    print("W state probabilities:\n", w_state_circuit())
---
+++ PongAgents/models2.py
@@ -0,0 +1,197 @@
+import pufferlib
+import torch
+from torch import nn
+from torch.distributions import Categorical
+
+import numpy as np
+
+from typing import List, Tuple, Union, Callable
+
+from VQCBackbones2 import (
+    GHZBackbone,
+    GraphStateBackbone,
+    SeparableBackbone,
+    WStateBackbone
+)
+
+BACKBONES = {
+    "ghz": GHZBackbone,
+    "graph_state": GraphStateBackbone,
+    "separable": SeparableBackbone,
+    "w_state": WStateBackbone
+}
+
+# EDGE_LIST = [(3, 0), (2, 0), (6, 0), (4, 0), (5, 0), (3, 1), (2, 1), (4, 1), (5, 1), (7, 1), (0, 1)]
+
+class ElementwiseScaleShift(nn.Module):
+    """
+    Element-wise affine transform: y = x * scale + shift
+
+    Parameters are shaped for broadcasting against the input. Typical usage is to
+    pass the feature dimension size so the parameters have shape (features,) and
+    apply along the last dimension, but any broadcastable shape works.
+
+    Args:
+        shape: int or tuple defining the parameter shape (broadcastable to inputs).
+        init_scale: initial value for scale (default 1.0).
+        init_shift: initial value for shift (default 0.0).
+        learnable: if True, scale/shift are learnable parameters; otherwise buffers.
+    """
+
+    def __init__(
+        self,
+        shape: Union[int, Tuple[int, ...]],
+        init_scale: float = 1.0,
+        init_shift: float = 0.0,
+        learnable: bool = True,
+    ) -> None:
+        super().__init__()
+        if isinstance(shape, int):
+            param_shape: Tuple[int, ...] = (shape,)
+        else:
+            param_shape = tuple(shape)
+
+        if learnable:
+            self.scale = nn.Parameter(torch.full(param_shape, float(init_scale)))
+            self.shift = nn.Parameter(torch.full(param_shape, float(init_shift)))
+        else:
+            self.register_buffer("scale", torch.full(param_shape, float(init_scale)))
+            self.register_buffer("shift", torch.full(param_shape, float(init_shift)))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x * self.scale + self.shift
+
+    def extra_repr(self) -> str:
+        return f"shape={tuple(self.scale.shape)}"
+
+class PongClassicalCritic(nn.Module):
+    def __init__(self, input_dim: int = 3, hidden_dim: int = 128):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, 1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = torch.relu(self.fc1(x))
+        return self.fc2(x)
+    
+class PongClassicalPolicy(nn.Module):
+    def __init__(self, input_dim: int, output_dim: int, hidden_dim: int = 128):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = torch.relu(self.fc1(x))
+        return self.fc2(x)
+
+class PongHybridAgent(nn.Module):
+    """
+    A hybrid agent with a classical critic and a quantum policy.
+    """
+    def __init__(self, agent_type, env, agent_args:dict):
+        """
+        :param agent_type: Type of the agent (e.g., "ghz", "graph_state", "separable").
+        :param env: The environment instance.
+        :param agent_args: Additional arguments for the agent (n_layers, post_select, edge_list)
+        """
+        super().__init__()
+        assert agent_type in BACKBONES, f"Unknown agent type: {agent_type}"
+        self.single_action_dim = env.single_action_space.n
+        self.observation_dim = env.single_observation_space.shape[0]
+        self.backbone = BACKBONES[agent_type](
+            output_dim=3,
+            observation_space=self.observation_dim,
+            **agent_args,
+        )
+        # Element-wise affine scaling of the quantum backbone output
+        self.affine = ElementwiseScaleShift(shape=3)
+        self.critic = PongClassicalCritic(input_dim=3)
+        self.actor = PongClassicalPolicy(
+            input_dim=3,
+            output_dim=self.single_action_dim,
+        )
+    
+    def get_representation(self, x):
+        hidden = self.backbone(x)
+        hidden = self.affine(hidden)
+        return hidden
+    
+    def get_value(self, x):
+        x = x * torch.pi * 2 # Scale inputs to [0, 2π]
+        hidden = self.get_representation(x)
+        return self.critic(hidden)
+
+    def get_action_and_value(self, x, action=None):
+        x = x * torch.pi * 2 # Scale inputs to [0, 2π]
+        hidden = self.get_representation(x)
+        logits = self.actor(hidden)
+        probs = Categorical(logits=logits)
+        if action is None:
+            action = probs.sample()
+        return action, probs.log_prob(action), probs.entropy(), self.critic(hidden)
+
+class PongClassicalAgent(nn.Module):
+    """
+    A classical agent with a classical critic.
+    """
+    def __init__(self, env):
+        super().__init__()
+        self.single_action_dim = env.single_action_space.n
+        self.observation_dim = env.single_observation_space.shape[0]
+        self.backbone = nn.Sequential(
+            nn.Linear(self.observation_dim, 2**8),
+            nn.ReLU(),
+            nn.Linear(2**8, 3)  # Output dimension for the critic
+        )
+        self.actor = PongClassicalPolicy(
+            input_dim=3,
+            output_dim=self.single_action_dim
+        )
+        self.critic = PongClassicalCritic(input_dim=3)
+
+    def get_representations(self, x):
+        hidden = self.backbone(x)
+        return hidden
+
+    def get_value(self, x):
+        hidden = self.get_representations(x)
+        return self.critic(hidden)
+
+    def get_action_and_value(self, x, action=None):
+        hidden = self.get_representations(x)
+        logits = self.actor(hidden)
+        probs = Categorical(logits=logits)
+        if action is None:
+            action = probs.sample()
+        return action, probs.log_prob(action), probs.entropy(), self.critic(hidden)
+
+if __name__ == "__main__":
+    from pufferlib import vector
+    from pufferlib.ocean import env_creator
+    import time
+
+    agent_type = "graph_state"
+    agent_args = {
+        "n_layers": 6,
+        "edge_list": [(3, 0), (2, 0), (6, 0), (4, 0), (5, 0), (3, 1), (2, 1), (4, 1), (5, 1), (7, 1), (0, 1)]
+    }
+
+    env_name = 'puffer_pong'
+    env_creator = env_creator(env_name)
+    vecenv = vector.make(env_creator, num_envs=2, num_workers=2, batch_size=1,
+        backend=vector.Multiprocessing, env_kwargs={'num_envs': 4096})
+    
+    obs, _ = vecenv.reset()
+    agent = PongHybridAgent(agent_type, vecenv, agent_args)
+    
+    test_count = 0
+    while True:
+        action, log_prob, entropy, value = agent.get_action_and_value(torch.tensor(obs))
+        obs, reward, terminated, truncated, info = vecenv.step(action.numpy())
+        print("Observation shape:", obs.shape)
+        print("Action shape:", action.shape)
+        time.sleep(1)
+        test_count += 1
+        if test_count > 10:
+            break
+    vecenv.close()
