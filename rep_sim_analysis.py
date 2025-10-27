6d72822bdf9c1d9fd8a0c5b7fdcff13929658317
Peiyong Wang <Peiyong.Wang@csiro.au>
2025-10-27 22:30:23 +1100

    Add similarity analysis script for representation evaluation

---
+++ rep_sim_analysis.py
@@ -0,0 +1,4 @@
+# Similarity analysis for the representations obtained from different backbones
+# Following the method described in: https://proceedings.mlr.press/v97/kornblith19a.html
+# "Similarity of Neural Network Representations Revisited"
+
