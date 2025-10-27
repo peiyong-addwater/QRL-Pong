f97de86aaa2db21d30872d27b3377bc4d6980203
Peiyong Wang <Peiyong.Wang@csiro.au>
2025-10-27 19:49:22 +1100

    Enhance representation generation and save paths to JSON file

--- get_reps.py
+++ get_reps.py
@@ -2,6 +2,7 @@ import os
 import re
 import torch
 import numpy as np
+import json
 
 from pufferlib import vector
 from pufferlib.ocean import env_creator
@@ -9,7 +10,7 @@ from pufferlib.ocean import env_creator
 from PongAgents import PongHybridAgent
 
 # device configuration
-device = torch.device("cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # path to the collected observations
 OBS_PATH = os.path.join("collected_obs", "obs_collection_Classical4096P.npy")
@@ -34,6 +35,12 @@ env_name = 'puffer_pong'
 env_creator = env_creator(env_name)
 envs = vector.make(env_creator, num_envs=1, num_workers=1, batch_size=1, backend=vector.Multiprocessing, env_kwargs={'num_envs': 128, 'log_interval':1})
 
+# a dictionary to store the path to the representations for each model
+reps_paths_dict = {}
+reps_paths_dict["separable"] = {}
+reps_paths_dict["entangled"] = {}
+reps_paths_dict["entangled_trainable_rzz"] = {}
+
 
 # generate representations for each model
 for model_file in model_files:
@@ -46,14 +53,31 @@ for model_file in model_files:
     if match:
         agent_type = match.group(1)
         num_layers = int(re.search(r'_QLayers_(\d+)', model_file).group(1))
-        print(f"Processing model: {model_file} | Agent Type: {agent_type} | Num Layers: {num_layers}")
+        
+        if num_layers not in reps_paths_dict[agent_type].keys():
+            reps_paths_dict[agent_type][num_layers] = []
+        
+        print(f"======Processing model: {model_file} | Agent Type: {agent_type} | Num Layers: {num_layers}======")
         agent = PongHybridAgent(agent_type=agent_type, env = envs, agent_args = {"n_layers": num_layers}).to(device)
+        
         print(f"Loading model from {model_path}")
         agent.load_state_dict(torch.load(model_path, map_location=device))
         print(f"Model loaded successfully.")
+        
         with torch.no_grad():
             # get representations
             reps = agent.get_representation(observations)
             print(f"Generated representations with shape: {reps.shape}")
+            # save representations
+            reps_path = os.path.join(REPS_FOLDER, f"{agent_type}_layers_{num_layers}.npy")
+            np.save(reps_path, reps.cpu().numpy())
+            print(f"Saved representations to {reps_path}")
+            reps_paths_dict[agent_type][num_layers].append(reps_path)
+
+# save the paths to the representations in a json file at the root of the project
+with open("reps_paths_dict.json", "w") as f:
+    json.dump(reps_paths_dict, f, indent=4)
+
+print("Saved representation paths to reps_paths_dict.json")
 
 
